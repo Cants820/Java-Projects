c288636a0b5be66fe332092800d5d0a1d1aadc5f
joseph-nxtek <joseph@nxtek.co>
2018-06-01 23:42:17 -0700

    upload

---
+++ odd-even-strings/odd-even-strings/src/odd/even/strings/OddEvenStrings.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package odd.even.strings;
+
+/**
+ *
+ * @author Joseph
+ */
+public class OddEvenStrings {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
